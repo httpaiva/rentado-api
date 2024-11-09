e479f05f7a716ed049229a16d63bff642e635e11
Gabriel Paiva <gabriel.paiva@pipefy.com>
2024-11-09 14:49:15 -0300

    fix: fix auth validation and create unit tests

--- src/auth/auth.controller.spec.ts
+++ src/auth/auth.controller.spec.ts
@@ -1,18 +1,100 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthController } from './auth.controller';
+import { UserService } from 'src/user/user.service';
+import { JwtService } from '@nestjs/jwt';
+import { UnauthorizedException } from '@nestjs/common';
+import * as bcrypt from 'bcrypt';
 
 describe('AuthController', () => {
-  let controller: AuthController;
+  let authController: AuthController;
+  let userService: UserService;
+  let jwtService: JwtService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [AuthController],
+      providers: [
+        {
+          provide: UserService,
+          useValue: {
+            findOneByEmail: jest.fn(),
+          },
+        },
+        {
+          provide: JwtService,
+          useValue: {
+            signAsync: jest.fn(),
+          },
+        },
+      ],
     }).compile();
 
-    controller = module.get<AuthController>(AuthController);
+    authController = module.get<AuthController>(AuthController);
+    userService = module.get<UserService>(UserService);
+    jwtService = module.get<JwtService>(JwtService);
   });
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
+  describe('signIn', () => {
+    it('should return an access token if credentials are valid', async () => {
+      const signInDto = { email: 'test@example.com', password: 'password123' };
+
+      const mockUser = {
+        id: 'user-id',
+        email: 'test@example.com',
+        password: 'hashedpassword123',
+      };
+
+      const mockToken = 'mocked-jwt-token';
+
+      // Mocking the UserService's method
+      userService.findOneByEmail = jest.fn().mockResolvedValue(mockUser);
+      jwtService.signAsync = jest.fn().mockResolvedValue(mockToken);
+      jest.spyOn(bcrypt, 'compareSync').mockReturnValue(true); // Simulate correct password comparison
+
+      const result = await authController.signIn(signInDto);
+
+      expect(result.access_token).toBe(mockToken);
+      expect(userService.findOneByEmail).toHaveBeenCalledWith(signInDto.email);
+      expect(bcrypt.compareSync).toHaveBeenCalledWith(
+        signInDto.password,
+        mockUser.password,
+      );
+      expect(jwtService.signAsync).toHaveBeenCalledWith({
+        sub: mockUser.id,
+        email: mockUser.email,
+      });
+    });
+
+    it('should throw UnauthorizedException if user not found', async () => {
+      const signInDto = { email: 'test@example.com', password: 'password123' };
+
+      userService.findOneByEmail = jest.fn().mockResolvedValue(null); // Simulating user not found
+
+      await expect(authController.signIn(signInDto)).rejects.toThrowError(
+        UnauthorizedException,
+      );
+      expect(userService.findOneByEmail).toHaveBeenCalledWith(signInDto.email);
+    });
+
+    it('should throw UnauthorizedException if password is incorrect', async () => {
+      const signInDto = { email: 'test@example.com', password: 'password123' };
+
+      const mockUser = {
+        id: 'user-id',
+        email: 'test@example.com',
+        password: 'hashedpassword123',
+      };
+
+      userService.findOneByEmail = jest.fn().mockResolvedValue(mockUser);
+      jest.spyOn(bcrypt, 'compareSync').mockReturnValue(false); // Simulate incorrect password comparison
+
+      await expect(authController.signIn(signInDto)).rejects.toThrowError(
+        UnauthorizedException,
+      );
+      expect(bcrypt.compareSync).toHaveBeenCalledWith(
+        signInDto.password,
+        mockUser.password,
+      );
+    });
   });
 });
--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -10,7 +10,7 @@ import { SignInDto } from './dto/sign-in.dto';
 import { Public } from 'src/utils/decorators/Public';
 import { UserService } from 'src/user/user.service';
 import { JwtService } from '@nestjs/jwt';
-import { compare } from 'bcrypt';
+import { compareSync } from 'bcrypt';
 
 @Controller('auth')
 export class AuthController {
@@ -25,14 +25,19 @@ export class AuthController {
   async signIn(@Body() signInDto: SignInDto) {
     const { email, password } = signInDto;
 
+    // Buscar usuário por e-mail
     const user = await this.userService.findOneByEmail(email);
     if (!user) {
       throw new UnauthorizedException();
     }
-    if (await compare(user.password, password)) {
+
+    // Comparar as senhas
+    const isPasswordValid = compareSync(password, user.password);
+    if (!isPasswordValid) {
       throw new UnauthorizedException('Invalid credentials');
     }
 
+    // Gerar o payload e o token JWT
     const payload = { sub: user.id, email: user.email };
     return {
       access_token: await this.jwtService.signAsync(payload),
